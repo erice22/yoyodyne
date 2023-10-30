8e5d11e548acbe66d3c52900983714538a9b137b
Enora <erice1@brynmawr.edu>
2023-10-30 15:14:17 -0600

    debug

--- yoyodyne/models/pointer_generator.py
+++ yoyodyne/models/pointer_generator.py
@@ -323,8 +323,7 @@ class PointerGeneratorLSTMEncoderDecoder(lstm.LSTMEncoderDecoder):
             projected_translation = F.dropout(self.tama_projection(avg_pooled), 0.3, self.training)
         else:
             projected_translation = None
-        encoder_output = self.source_encoder(batch, projected_translation)
-        print(type(self.source_encoder))
+        encoder_output = self.source_encoder(batch, projected_translation).output
         source_encoded = encoder_output.output
         if encoder_output.has_hiddens:
             h_source, c_source = encoder_output.hiddens
